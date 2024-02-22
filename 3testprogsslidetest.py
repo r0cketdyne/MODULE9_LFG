f0a4ae9ef6fd2587fb106d54fdfb92a62ab6f2a5
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-22 09:18:08 -0500

    BEEP

--- 3testprogsslidetest.py
+++ 3testprogsslidetest.py
@@ -11,3 +11,8 @@ Created on Thu Feb 22 09:05:53 2024
 #and just comment them out if they won't be run,
 #if I need to test them I'll just make sure
 #they're uncommended
+#actually I need to go to a Physics talk at McGill.
+#mostly because I myself am giving a talk next week
+#and need to prep; will study the format
+#and presentation style.
+#will be back at this soon.
