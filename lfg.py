289134d86e213d8e435b9519a6689e905b441a33
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-21 20:44:57 -0500

    list comprehension

--- lfg.py
+++ lfg.py
@@ -1,1 +1,1 @@
-#jumping right into mod 3; post exam.#prompt the user for a string and count the number of vowels in the stringusr_str = input("Throw me some data mf...hungry for pretty much any word\n")slicer = [char for char in usr_str]print(slicer)
+#jumping right into mod 3; post exam.#prompt the user for a string and count the number of vowels in the stringusr_str = input("Throw me some data mf...hungry for pretty much any word\n")slicer = [char for char in usr_str]#so; this is a for loop literally constrained within an arr. a list comprehensionprint(slicer)
