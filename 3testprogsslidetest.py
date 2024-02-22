1cd37afd65fa440982f5117dcae5e7178aa2a00f
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-02-22 09:07:28 -0500

    LFG

---
+++ 3testprogsslidetest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 09:05:53 2024
+
+@author: stephenson
+"""
+
+#I'm going to write solutions for like three of the
+#practice problems at the same python file
+#and just comment them out if they won't be run,
+#if I need to test them I'll just make sure
+#they're uncommended
